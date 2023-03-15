3db0d56cedf0715c02e6690b8e4842e32d9602d5
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-03-14 18:47:37 -0700

    fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,7 @@
-
+import EmailIcon from '@mui/icons-material/Email';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Footer = (props) => {
     
     return (
@@ -8,16 +11,16 @@ const Footer = (props) => {
             <div class="col-1-of-2">
                 <div class="footer__navigation">
                     <ul class="footer__list">
-                        <li class="footer__item"><a href="https://www.linkedin.com/in/jeffrey-aeschliman-054330253/" class="footer__link">LinkedIn</a></li>
-                        <li class="footer__item"><a href="https://github.com/JeffreyAes" class="footer__link">Github</a></li>
-                        <li class="footer__item"><a href="https://www.youtube.com/channel/UC6nomRb0o3MvBJCpo8HhL4A" class="footer__link">Youtube</a></li>
-                        <li class="footer__item"><a href="mailto:aeschlimanjeffrey@gmail.com" class="footer__link">Email</a></li>
+                        <li class="footer__item"><a href="https://www.linkedin.com/in/jeffrey-aeschliman-054330253/" class="footer__link"><LinkedInIcon style={{ fontSize: 50 }} /></a></li>
+                        <li class="footer__item"><a href="https://github.com/JeffreyAes" class="footer__link"><GitHubIcon style={{ fontSize: 50 }}/></a></li>
+                        <li class="footer__item"><a href="https://www.youtube.com/channel/UC6nomRb0o3MvBJCpo8HhL4A" class="footer__link"><YouTubeIcon style={{ fontSize: 50 }}/></a></li>
+                        <li class="footer__item"><a href="mailto:aeschlimanjeffrey@gmail.com" class="footer__link"><EmailIcon style={{ fontSize: 50 }}/></a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-1-of-2">
                 <p class="footer__copyright u-center-text">
-                © Copyright 2023. Made by  <a href="#home" className="footer__link">Jeffrey Aeschliman</a>
+                © Copyright 2023. Made by  <a href="#home" className="footer__link--name">Jeffrey Aeschliman</a>
                 </p>
             </div>
         </div>
